9df66eacb277d98fefbdccd90ec27cf4d10ed706
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-25 12:15:06 -0400

    update

--- back/schemas/index.js
+++ back/schemas/index.js
@@ -1,7 +1,8 @@
 import user from "./user"
 import comment from "./comment"
+import note from "./note"
+import postedBy from "./postedBy"
+import save from "./save"
 
 
-
-
-export const schemaTypes = [user, comment]
+export const schemaTypes = [user, comment, note, postedBy, save]
