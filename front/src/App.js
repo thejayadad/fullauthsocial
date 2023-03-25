be275cef954ff222d59de57c108fec51e4c4bce6
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-25 12:32:50 -0400

    update

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 
+
 function App() {
   return (
     <div>
--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import SocialButton from "../components/SocialLogin.js";
+
+
+
 
 const Login = () => {
+const handleSocialLogin = (user) => {
+    console.log(user);
+    };
+
+    const handleSocialLoginFailure = (err) => {
+    console.error(err);
+    };
+
   return (
-    <div>Login</div>
+    <div>Login
+
+        <SocialButton
+        provider="google"
+        onLoginSuccess={handleSocialLogin}
+        onLoginFailure={handleSocialLoginFailure}
+        clientId={process.env.GOOGLE__API_CLIENT_ID}
+        >
+            Login with Google
+        </SocialButton>
+    </div>
   )
 }
 
