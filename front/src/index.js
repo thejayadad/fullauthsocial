ea063a081069c03e4d48326245bf2f4bedf7ff9c
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-25 14:22:19 -0400

    update

--- front/src/index.js
+++ front/src/index.js
@@ -9,7 +9,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="29160364349-bk47uk9pra6jq5p2a35k34oh2nvmbe41.apps.googleusercontent.com">
+    <GoogleOAuthProvider c
     <BrowserRouter>
       <App />
       </BrowserRouter>
